52bf4249dce3c6b9ef87902e4688a8222e8d1b60
jeanagnissan <jean@digital.ci>
2022-12-31 18:45:14 +0000

    update code in js file

--- script1.js
+++ script1.js
@@ -1,16 +1,30 @@
 
 
-let sentence = "The movie is not that bad, I like it ";
+/**
+ * Exercices 1
+ */
+ console.log("-----------------Exercices 1---------------");
 
+// 1- Créez une variable appelée sentence. La valeur de la variable doit être une chaîne contenant les mots « not » et « bad ».
+let sentence = "The movie is not that bad , I like it ";
 console.log(sentence);
 
+// 2- Créez une variable appelée wordNotoù sa valeur est la première apparition (c'est-à-dire la position) de la sous-chaîne "not" (de la sentencevariable).
 let wordNot =  sentence.indexOf("not");
-let wordBad = sentence.indexOf("bad");
+console.log(wordNot);
 
+// 3- Créez une variable appelée wordBadoù sa valeur est la première apparition (c'est-à-dire la position) de la sous-chaîne "bad" (de la sentencevariable).
+let wordBad = sentence.indexOf("bad");
+console.log(wordBad);
+
+// 4- i le mot "bad" vient après le mot "not", vous devez remplacer toute la sous-chaîne "not…bad" par "good", puis console.log le résultat.
 if( wordNot < wordBad){
     let replaceSubString = sentence.replace("not that bad", "good")
     console.log(replaceSubString);
-} else if( wordNot > wordBad || wordNot == -1 && wordNot == -1){
+}
+
+// 5- Si le mot "bad" ne vient pas après "not" ou si les mots ne sont pas dans la phrase, console.log la phrase originale.
+if( wordNot > wordBad || wordNot == -1 || wordBad == -1){
     console.log(sentence);
 }
 
